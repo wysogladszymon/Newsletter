b51b4bc8b4fac0da4e2c6fb746dfe40e100744e4
szymonwysoglad <szymon.wysoglad@medius.com>
2024-08-02 15:16:11 +0200

    beginning of readme.md

--- README.md
+++ README.md
@@ -14,7 +14,42 @@
 ![Zdjęcie 4](https://github.com/user-attachments/assets/c3b9de73-1218-43df-902b-0a6b59a679e7)
 *Zdjęcie 4: Wiadomość w skrzynce użytkownika.*
 
-## Setup
+## Dodawanie klucza ssh na windows 
+1. Wygeneruj klucz ssh, z twoim emailem (tym, który masz podpięty do konta ```github```)
+```shell
+ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+```
+2. Upewnij się, że agent ssh jest uruchomiony:
+```shell
+eval $(ssh-agent -s)
+```
+3. Dodaj klucz SSh do agenta
+```shell
+ssh-add C:\Users\your_username\.ssh\id_rsa
+```
+gdzie ```your_username``` trzeba podmienić nazwą uzytkownika windows. 
 
+4. Wyświetl klucz ssh, aby go skopiować i dodać do swoich kluczy ssh w github:
+```shell
+cat C:\Users\your_username\.ssh\id_rsa.pub
+```
+## Setup
+1. Otwórz powershell z uprawnieniami administratora
+2. Upewnij się, że masz zainstalowane ```chocolatey```, możesz wpisać komendę:
+```shell
+choco --version
+```
+Jeżeli nie masz, to zainstaluj je z tej strony: [Link do pobrania chocolatey](https://chocolatey.org/install#psdsc)
+
+3. Zainstaluj git:
+```shell
+choco install git -y
+```
+4. Dodaj klucze ssh do twojego komputera, oraz zrób to samo w platformie [```github```](github.com), instruktaż można znaleźć tu:
+
+5. Sklonuj repozytorium:
+```shell
+git clone git@github.com:wysogladszymon/Newsletter.git
+```
 ## Uruchomienie
 
--- install.sh
+++ install.sh
@@ -1,38 +1,65 @@
 #!/bin/sh
 
-$pythonVersion="3.12.4"
-$nodeVersion="22.5.1"
-# install wget
-echo "installing wget"
+pythonVersion="3.12.4"
+nodeVersion="22.5.1"
+
+# Install wget
+echo "Installing wget..."
 choco install wget -y
 
-# download python using chocolatey
-echo "installing python"
-choco install python --version=$pythonVersion
+# Install git
+echo "Installing git..."
+choco install git -y
 
-# node installation
-echo "installing nodejs"
-choco install nodejs --version="$nodeVersion"
+# Download Python using Chocolatey
+echo "Installing Python..."
+choco install python --version=$pythonVersion -y
 
-# pnpm installation
-echo "installing pnpm"
-npm install pnpm
+# Node.js installation
+echo "Installing Node.js..."
+choco install nodejs --version=$nodeVersion -y
 
-# configure frontend
+# PNPM installation
+echo "Installing PNPM..."
+npm install -g pnpm
+
+# Clone the project repository
+echo "Cloning the project repository..."
+git clone <url_twojego_repozytorium>
+cd <nazwa_katalogu_projektu>
+
+# Configure frontend
+echo "Configuring frontend..."
 cd AdmPage/frontend
 pnpm install
+pnpm build
 
-#configure backend
+# Configure backend Python
+echo "Configuring backend (Python)..."
 cd ../backend
 python -m venv env
 .\env\Scripts\activate
 
+pip install --upgrade pip
 pip install python-dotenv
 pip install fastapi
 pip install pymongo
+pip install uvicorn
 
-# instalacja dotnet
+# Install .NET SDK and Visual Studio Code
+echo "Installing .NET SDK and Visual Studio Code..."
 choco install dotnet-sdk -y
 choco install vscode -y
 
+# Install C# extension for VS Code
+echo "Installing C# extension for Visual Studio Code..."
 code --install-extension ms-dotnettools.csharp
+
+# Restore .NET dependencies
+echo "Restoring .NET dependencies..."
+cd ../../joinPage/backend/backend
+dotnet restore
+
+# Finish
+cd ../../../
+echo "Installation and setup completed."
